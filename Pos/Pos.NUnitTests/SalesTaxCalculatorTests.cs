50d45f4eedb682b6b8c326f9505def5fac5114af
unknown <BFriesen@bfriesen81vm.mi.corp.rockfin.com>
2014-11-12 16:30:45 -0500

    Add nunit tests.

---
+++ Pos/Pos.NUnitTests/SalesTaxCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos.NUnitTests
+{
+    [TestFixture]
+    public class SalesTaxCalculatorTests
+    {
+        [TestCase(State.MI, 6)]
+        [TestCase(State.IL, 7)]
+        [TestCase(State.NY, 7)]
+        [TestCase(State.TN, 11)]
+        public void StatesUseTheAppropriateTaxRate(State state, int expectedTaxAmount)
+        {
+            // Arrange
+            decimal amount = 100;
+
+            SalesTaxCalculator calculator = new SalesTaxCalculator();
+
+            // Act
+            decimal result = calculator.Calculate(amount, state);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedTaxAmount));
+        }
+
+        [Test]
+        public void InvalidStateThrowsException()
+        {
+            // Arrange
+            State state = (State)(-1);
+            decimal amount = 1;
+
+            SalesTaxCalculator calculator = new SalesTaxCalculator();
+
+            // Act
+            Assert.Throws(typeof(Exception), () => calculator.Calculate(amount, state));
+        }
+    }
+}
