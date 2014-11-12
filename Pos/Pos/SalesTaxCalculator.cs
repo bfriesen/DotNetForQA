7466a2000970e9edf70b067cf4c3179e439c1b59
unknown <BFriesen@bfriesen81vm.mi.corp.rockfin.com>
2014-11-12 09:15:20 -0500

    Initial commit.

---
+++ Pos/Pos/SalesTaxCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos
+{
+    public class SalesTaxCalculator
+    {
+        private readonly decimal _taxRate;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SalesTaxCalculator"/> class.
+        /// </summary>
+        /// <param name="taxRatePercent">
+        /// The tax rate percent. For example, 7.25M for 7.25%. Not 0.0725M.
+        /// </param>
+        public SalesTaxCalculator(decimal taxRatePercent)
+        {
+            _taxRate = taxRatePercent / 100;
+        }
+
+        public decimal Calculate(decimal amount)
+        {
+            return amount * _taxRate;
+        }
+    }
+}
