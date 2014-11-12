13146a2f957df047734ebc32aef19fb09b7d722c
unknown <BFriesen@bfriesen81vm.mi.corp.rockfin.com>
2014-11-12 10:24:14 -0500

    Stub out sales tax calculator form.

---
+++ Pos/Pos.UI/SalesTaxCalculatorForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Pos.UI
+{
+    partial class SalesTaxCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.lblState = new System.Windows.Forms.Label();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.btnCalculateTax = new System.Windows.Forms.Button();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblTaxAmount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(49, 34);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(43, 13);
+            this.lblAmount.TabIndex = 0;
+            this.lblAmount.Text = "Amount";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(99, 34);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(60, 78);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(32, 13);
+            this.lblState.TabIndex = 2;
+            this.lblState.Text = "State";
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(99, 75);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(100, 20);
+            this.txtState.TabIndex = 3;
+            // 
+            // btnCalculateTax
+            // 
+            this.btnCalculateTax.Location = new System.Drawing.Point(99, 169);
+            this.btnCalculateTax.Name = "btnCalculateTax";
+            this.btnCalculateTax.Size = new System.Drawing.Size(100, 23);
+            this.btnCalculateTax.TabIndex = 4;
+            this.btnCalculateTax.Text = "Calculate Tax";
+            this.btnCalculateTax.UseVisualStyleBackColor = true;
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Location = new System.Drawing.Point(67, 117);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(25, 13);
+            this.lblTax.TabIndex = 5;
+            this.lblTax.Text = "Tax";
+            // 
+            // lblTaxAmount
+            // 
+            this.lblTaxAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTaxAmount.Location = new System.Drawing.Point(98, 112);
+            this.lblTaxAmount.Name = "lblTaxAmount";
+            this.lblTaxAmount.Size = new System.Drawing.Size(101, 23);
+            this.lblTaxAmount.TabIndex = 6;
+            this.lblTaxAmount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 236);
+            this.Controls.Add(this.lblTaxAmount);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.btnCalculateTax);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.lblAmount);
+            this.Name = "Form1";
+            this.Text = "Sales Tax Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.Button btnCalculateTax;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblTaxAmount;
+    }
+}
+
--- Pos/Pos.UI/SalesTaxCalculatorForm.cs
+++ Pos/Pos.UI/SalesTaxCalculatorForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Pos.UI
 {
-    public partial class Form1 : Form
+    public partial class SalesTaxCalculatorForm : Form
     {
-        public Form1()
+        public SalesTaxCalculatorForm()
         {
             InitializeComponent();
         }
